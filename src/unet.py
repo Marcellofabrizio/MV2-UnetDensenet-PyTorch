b2c539a584c32cb57f14a4280cb8027cc3513aa0
Marcello Fabrizio <marcfabrizio10@gmail.com>
2024-09-22 15:36:32 -0300

    Remove copied code

--- src/unet.py
+++ src/unet.py
@@ -112,60 +112,6 @@ class Upsample2DBlock(nn.Module):
         x = self.relu(x)
         return x
 
-class UpsampleBlock(nn.Module):
-
-    # TODO: separate parametric and non-parametric classes?
-    # TODO: skip connection concatenated OR added
-
-    def __init__(self, ch_in, ch_out=None, skip_in=0, use_bn=True, parametric=False):
-        super(UpsampleBlock, self).__init__()
-
-        self.parametric = parametric
-        ch_out = ch_in/2 if ch_out is None else ch_out
-
-        # first convolution: either transposed conv, or conv following the skip connection
-        if parametric:
-            # versions: kernel=4 padding=1, kernel=2 padding=0
-            self.up = nn.ConvTranspose2d(in_channels=ch_in, out_channels=ch_out, kernel_size=(4, 4),
-                                         stride=2, padding=1, output_padding=0, bias=(not use_bn))
-            self.bn1 = nn.BatchNorm2d(ch_out) if use_bn else None
-        else:
-            self.up = None
-            ch_in = ch_in + skip_in
-            self.conv1 = nn.Conv2d(in_channels=ch_in, out_channels=ch_out, kernel_size=(3, 3),
-                                   stride=1, padding=1, bias=(not use_bn))
-            self.bn1 = nn.BatchNorm2d(ch_out) if use_bn else None
-
-        self.relu = nn.ReLU(inplace=True)
-
-        # second convolution
-        conv2_in = ch_out if not parametric else ch_out + skip_in
-        self.conv2 = nn.Conv2d(in_channels=conv2_in, out_channels=ch_out, kernel_size=(3, 3),
-                               stride=1, padding=1, bias=(not use_bn))
-        self.bn2 = nn.BatchNorm2d(ch_out) if use_bn else None
-
-    def forward(self, x, skip_connection=None):
-
-        x = self.up(x) if self.parametric else F.interpolate(x, size=None, scale_factor=2, mode='bilinear',
-                                                             align_corners=None)
-        if self.parametric:
-            x = self.bn1(x) if self.bn1 is not None else x
-            x = self.relu(x)
-
-        if skip_connection is not None:
-            x = torch.cat([x, skip_connection], dim=1)
-
-        if not self.parametric:
-            x = self.conv1(x)
-            x = self.bn1(x) if self.bn1 is not None else x
-            x = self.relu(x)
-        x = self.conv2(x)
-        x = self.bn2(x) if self.bn2 is not None else x
-        x = self.relu(x)
-
-        return x
-
-
 class UnetDensenet(nn.Module):
     def __init__(
         self,
