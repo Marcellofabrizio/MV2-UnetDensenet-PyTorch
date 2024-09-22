aef275bc0747759320aab9a5bbce23942336f562
Marcello Fabrizio <marcfabrizio10@gmail.com>
2024-09-22 15:35:52 -0300

    Working

--- src/unet.py
+++ src/unet.py
@@ -12,8 +12,6 @@ class UNetConvBnReLU(nn.Module):
         out_channels, 
         parametric=False,
         kernel_size=(4, 4), 
-        strides=(1, 1), 
-        padding='same',
     ):
         super(UNetConvBnReLU, self).__init__()
         
@@ -37,24 +35,81 @@ class UNetConvBnReLU(nn.Module):
         return x
 
 class Upsample2DBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, skip_in=0, kernel_size=(3,3), upsample_rate=(2,2)):
+    def __init__(self, ch_in, ch_out=None, skip_in=0, parametric=False):
+        """
+        Upsample2DBlock performs either parametric (ConvTranspose2d) or non-parametric 
+        (bilinear interpolation + Conv2d) upsampling, with optional skip connections.
+        
+        Args:
+        - ch_in: Input channel size.
+        - ch_out: Output channel size. Defaults to half of ch_in if not provided.
+        - skip_in: Additional input channels from the skip connection.
+        - parametric: If True, use ConvTranspose2d for upsampling, otherwise use interpolation.
+        """
         super(Upsample2DBlock, self).__init__()
-        self.upsample = nn.Upsample(scale_factor=upsample_rate, mode='bilinear', align_corners=True)
-        
-        self.conv1 = UNetConvBnReLU(in_channels, out_channels, parametric=True, kernel_size=kernel_size)
-        self.conv2 = UNetConvBnReLU(in_channels, out_channels+skip_in, kernel_size=kernel_size)
 
-    def forward(self, x, skip=None):
-        x = self.upsample(x)
+        ch_out = ch_in // 2 if ch_out is None else ch_out
+
+        self.parametric = parametric
+
+        if parametric:
+            self.up = nn.ConvTranspose2d(ch_in, ch_out, kernel_size=4, stride=2, padding=1)
+            self.conv1 = None
+            self.bn1 = None
+        else:
+            self.up = None
+            conv1_in = ch_in + skip_in if not parametric else ch_out
+            self.conv1 = nn.Conv2d(conv1_in, ch_out, kernel_size=3, padding=1)
+            self.bn1 = nn.BatchNorm2d(ch_out)
+            
+
+        conv2_in = ch_out + skip_in if parametric else ch_out
+        self.conv2 = nn.Conv2d(conv2_in, ch_out, kernel_size=3, padding=1)
+
+        self.bn2 = nn.BatchNorm2d(ch_out)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x, skip_connection=None):
+        """
+        Forward pass of the Upsample2DBlock.
         
-        if skip is not None:
-            print('Concatenating {} with {}'.format(x.shape, skip.shape))
-            x = torch.cat([x, skip], dim=1)
+        Args:
+        - x: Input tensor.
+        - skip_connection: Skip connection tensor from an earlier layer, if available.
         
-        print('Size after concat {}'.format(x.shape))
+        Returns:
+        - x: Output tensor after upsampling and convolutions.
+        """
+        if self.parametric:
+            x = self.up(x)
+        else:
+            x = F.interpolate(x, size=None, scale_factor=2, mode='bilinear', align_corners=False)
+
+        if skip_connection is not None:
+            x = torch.cat([x, skip_connection], dim=1)
+        x = self._conv_bn_relu(self.conv1, self.bn1, x)
+
+        x = self._conv_bn_relu(self.conv2, self.bn2, x)
+        return x
+
+    def _conv_bn_relu(self, conv, bn, x):
+        """
+        Helper function to apply convolution, batch normalization (if applicable), and ReLU activation.
         
-        x = self.conv1(x)
-        x = self.conv2(x)
+        Args:
+        - conv: Convolution layer to apply.
+        - bn: Batch Normalization layer (optional).
+        - x: Input tensor.
+        
+        Returns:
+        - x: Output tensor after applying conv, bn, and ReLU.
+        """
+        
+        if conv is not None :
+            x = conv(x)
+        if bn is not None:
+            x = bn(x)
+        x = self.relu(x)
         return x
 
 class UpsampleBlock(nn.Module):
@@ -123,14 +178,7 @@ class UnetDensenet(nn.Module):
         super(UnetDensenet, self).__init__()
         
         self.backbone, self.shortcut_features, self.bb_out_name = self.get_backbone()
-        
-        print('Shortcut features: {}\nBackbone output names: {}'.format(self.shortcut_features, self.bb_out_name))
-        
         shortcut_chs, bb_out_chs = self.infer_skip_channels()
-
-        print('Shortcut Channels: {}\nBackbone output channels: {}'.format(shortcut_chs, bb_out_chs))
-
-        filters = [1024, 512, 256, 128, 64]
 
         self.upsample_blocks = nn.ModuleList()
         decoder_filters = decoder_filters[:len(self.shortcut_features)]
@@ -143,30 +191,23 @@ class UnetDensenet(nn.Module):
             print('upsample_blocks[{}]\nin: {}\nout: {}\n'.format(i, filters_in, filters_out))
             skip_in=shortcut_chs[num_blocks-i-1]
             print('skip_in {}'.format(skip_in))
-            # self.upsample_blocks.append(Upsample2DBlock(filters_in, filters_out))
-            self.upsample_blocks.append(UpsampleBlock(filters_in, filters_out,
+            self.upsample_blocks.append(Upsample2DBlock(filters_in, filters_out,
                                                       skip_in=skip_in,
-                                                      parametric=True,
-                                                      use_bn=True))
-
+                                                      parametric=True))
         
-        self.final_conv = nn.Conv2d(decoder_filters[-1], 1, kernel_size=(3, 3))
+        self.final_conv = nn.Conv2d(decoder_filters[-1], 1, kernel_size=(1, 1))
 
     def forward(self, *input):
-
         x, features = self.forward_backbone(*input)
 
         for skip_name, upsample_block in zip(self.shortcut_features[::-1], self.upsample_blocks):
             skip_features = features[skip_name]
-            print('X Shape: {}'.format(x.shape))
-            
             x = upsample_block(x, skip_features)
 
         x = self.final_conv(x)
         return x
     
     def forward_backbone(self, x):
-
         features = {None: None} if None in self.shortcut_features else dict()
         for name, child in self.backbone.named_children():
             x = child(x)
